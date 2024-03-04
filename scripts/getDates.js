269e8c282eca3a09a50a1092e9511938229bdf58
user <ulises.a.rivero.garcia@gmail.com>
2024-03-04 02:20:37 -0600

    Course home page finished

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+//current year
+const year = {year: 'numeric'};
+document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', year);
+
+//Function for the last modification date
+document.querySelector('#lastUpdate').textContent = document.lastModified;
