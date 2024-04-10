7e6d39cf92fb364bdf833adb6408dd5e3927363b
user <ulises.a.rivero.garcia@gmail.com>
2024-04-10 00:08:12 -0600

    Adjusted the weatherAPI

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -27,7 +27,6 @@ modeButton.addEventListener("click", () => {
 //Weather API
 const clima = document.querySelector("#weatherToluca");
 const imagenClima = document.querySelector("#weather-icon");
-const subtitulo = document.querySelector("figcaption");
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=19.29&lon=-99.65&appid=01ccf5847d3489282a252a11305c78dd&units=imperial';
 
@@ -36,7 +35,7 @@ async function buscarAPI() {
         const respuesta = await fetch(url);
         if (respuesta.ok) {
             const datos = await respuesta.json();
-            //console.log(datos); //testing only
+            console.log(datos); //testing only
             mostrarResultados(datos);
         } else {
             throw Error (await respuesta.text());
@@ -49,10 +48,8 @@ async function buscarAPI() {
 buscarAPI();
 
 function mostrarResultados(datos) {
-    clima.innerHTML = `${datos.main.temp}&degF`;
+    clima.innerHTML = `${datos.main.temp}&degF - ${datos.weather[0].description}`;
     const icono = `https://openweathermap.org/img/w/${datos.weather[0].icon}.png`;
-    const desc = datos.weather[0].description;
     imagenClima.setAttribute('src', icono);
-    imagenClima.setAttribute('alt', desc);
-    subtitulo.textContent = `${desc}`;
+    imagenClima.setAttribute('alt', datos.weather[0].description);
 }
