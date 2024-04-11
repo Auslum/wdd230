0b37814ea15393cf192c0549516fcb46790e1238
user <ulises.a.rivero.garcia@gmail.com>
2024-04-10 23:15:38 -0600

    fixed console errors in directory JS

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -61,69 +61,74 @@ function obtainTimestamp() {
 }
 
 //The directory part
-const baseURL = "https://auslum.github.io/wdd230/";
-const linksURL = "https://auslum.github.io/wdd230/chamber/data/members.json";
-const art1 = document.querySelector(".grid");
+if (document.querySelector('.directoryMain') !== null) {
+	const baseURL = "https://auslum.github.io/wdd230/";
+	const linksURL = "https://auslum.github.io/wdd230/chamber/data/members.json";
+	const art1 = document.querySelector(".grid");
 
-async function getJson() {
-	const respuesta = await fetch(linksURL);
-	const datos = await  respuesta.json();
-	console.log(datos.members);
-	displayDirectory(datos.members);
+	async function getJson() {
+		const respuesta = await fetch(linksURL);
+		const datos = await  respuesta.json();
+		//console.log(datos.members);
+		displayDirectory(datos.members);
+	}
+
+	getJson();
+
+	const displayDirectory = (members) => {
+		members.forEach((miembro) => {
+			let logo = document.createElement('img');
+			let nombres = document.createElement('h3');
+			let direccion = document.createElement('p');
+			let telefono = document.createElement('p');
+			let liga = document.createElement('a');
+			let nivel = document.createElement('p');
+			let seccion = document.createElement('section');
+			
+			logo.setAttribute('src', miembro.iconFile);
+			logo.setAttribute('alt', `${miembro.name} logo`);
+			logo.setAttribute('loading', 'lazy');
+			logo.setAttribute('width', '120');
+			logo.setAttribute('height', '120');
+
+			nombres.textContent = `${miembro.name}`;
+			direccion.textContent = `${miembro.address}`;
+			telefono.textContent = `${miembro.phoneNumber}`;
+			liga.textContent = `${miembro.url}`;
+			liga.setAttribute('href', miembro.url);
+			nivel.textContent = `${miembro.membershipLvl}`;
+			
+			seccion.appendChild(logo);
+			seccion.appendChild(nombres);
+			seccion.appendChild(direccion);
+			seccion.appendChild(telefono);
+			seccion.appendChild(liga);
+			art1.appendChild(seccion);
+
+		})
+	}
 }
-
-getJson();
-
-const displayDirectory = (members) => {
-	members.forEach((miembro) => {
-		let logo = document.createElement('img');
-		let nombres = document.createElement('h3');
-		let direccion = document.createElement('p');
-		let telefono = document.createElement('p');
-		let liga = document.createElement('a');
-		let nivel = document.createElement('p');
-		let seccion = document.createElement('section');
-		
-		logo.setAttribute('src', miembro.iconFile);
-		logo.setAttribute('alt', `${miembro.name} logo`);
-		logo.setAttribute('loading', 'lazy');
-		logo.setAttribute('width', '120');
-		logo.setAttribute('height', '120');
-
-		nombres.textContent = `${miembro.name}`;
-		direccion.textContent = `${miembro.address}`;
-		telefono.textContent = `${miembro.phoneNumber}`;
-		liga.textContent = `${miembro.url}`;
-		liga.setAttribute('href', miembro.url);
-		nivel.textContent = `${miembro.membershipLvl}`;
-		
-		seccion.appendChild(logo);
-		seccion.appendChild(nombres);
-		seccion.appendChild(direccion);
-		seccion.appendChild(telefono);
-		seccion.appendChild(liga);
-		art1.appendChild(seccion);
-
-	})
-}
-
 //Code for grid and list views in directory
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+if (document.querySelector('.directoryMain') !== null) {
+	const gridbutton = document.querySelector("#grid");
+	const listbutton = document.querySelector("#list");
+	const display = document.querySelector("article");
 
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
+	gridbutton.addEventListener("click", () => {
+		// example using arrow function
+		display.classList.add("grid");
+		display.classList.remove("list");
+	});
+	
+	listbutton.addEventListener("click", showList); // example using defined function
+	
+	function showList() {
+		display.classList.add("list");
+		display.classList.remove("grid");
+	}
 }
+
+
 
 
